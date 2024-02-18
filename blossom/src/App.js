cb0211c933d95482bad351324de203b5b3a6a873
manikumarchintapall <157659942+manikumarchintapall@users.noreply.github.com>
2024-02-18 13:38:18 -0500

    Added few more components

--- blossom/src/App.js
+++ blossom/src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
-
+import Navbar from './Navbar'; // Import the Navbar component
 function App() {
   return (
     <div>
+      <Navbar />
       <h1>Hello World</h1>
     </div>
   );
---
+++ blossom/src/components/Florist.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const This is Florist entry point = () => {
+  return (
+    <div>This is Florist entry point</div>
+  )
+}
