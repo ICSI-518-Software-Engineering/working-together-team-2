3f4a81d26aeb605ddbc976aa0d2c5ce873cfb948
ggirugu <giridharsaigirugu@gmail.com>
2024-04-27 15:38:33 -0400

    Added customer order history route

--- Blossom/backend/routes/index.js
+++ Blossom/backend/routes/index.js
@@ -1,11 +1,13 @@
 import orderRouter from "./orderRoutes.js";
 import productRouter from "./productRoutes.js";
 import userRouter from "./userRoutes.js";
-import cartRouter from "./cartRoute.js"
+import cartRouter from "./cartRoute.js";
+import emailRouter from "./emailRoute.js";
 
 export const initializeRoutes = (app) => {
   app.use("/api/auth", userRouter);
   app.use("/api/products", productRouter);
   app.use("/api/order", orderRouter);
   app.use("/api/cart", cartRouter);
+  app.use("/api/email", emailRouter);
 };
